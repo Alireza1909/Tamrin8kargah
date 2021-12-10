289aa120842916b8f2db8dab455e7af2b6dcd43c
Alireza1909 <94298361+Alireza1909@users.noreply.github.com>
2021-12-11 01:45:51 +0330

    Add files via upload
    تمرین سری 8 کارگاه کامپیوتر-دانشگاه صنعتی شریف

---
+++ java.js
@@ -0,0 +1,10 @@
+function f(){
+    document.getElementById('user');
+}
+function ersalnahaei(){
+    var a;
+    a=confirm("ایا از ارسال فرم اطمینان دارید؟");
+    if (a){
+        alert('عملیات موفقیت آمیز بود.');
+    }
+}
